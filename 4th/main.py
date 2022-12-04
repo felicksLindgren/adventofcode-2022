fe4459adffee4ef4527826915ddfe90e460e24c8
Felix Lindgren <felix.p.lindgren@gmail.com>
2022-12-04 15:04:21 +0000

    Day 4

---
+++ 4th/main.py
@@ -0,0 +1,48 @@
+
+def Part1(pairs): 
+    total = 0
+
+    for pair in pairs:
+        arr = pair.split(',')
+        first =  [int(numeric_string) for numeric_string in arr[0].split('-')]     
+        second =  [int(numeric_string) for numeric_string in arr[1].split('-')]
+
+        sumOfFirst = int(first[1]) - int(first[0])
+        sumOfSecond = int(second[1]) - int(second[0])
+
+
+        if sumOfFirst == sumOfSecond:
+            if first[0] == second[0]:
+                total += 1
+        if sumOfFirst > sumOfSecond:
+            if second[0] >= first[0] and second[1] <= first[1]:
+                total += 1
+        if sumOfFirst < sumOfSecond:
+            if first[0] >= second[0] and first[1] <= second[1]:
+                total += 1
+
+    print(total)
+
+def Part2(pairs):
+    total = 0
+
+    for pair in pairs:
+        arr = pair.split(',')
+        first =  [int(numeric_string) for numeric_string in arr[0].split('-')]     
+        second =  [int(numeric_string) for numeric_string in arr[1].split('-')]
+
+        firstNumbers = list(range(first[0], first[1] + 1))
+        secondNumbers = list(range(second[0], second[1] + 1))
+
+        matches = set(firstNumbers) & set(secondNumbers)
+
+        if matches:
+            total += 1
+
+    print(total)
+
+if __name__ == "__main__":
+    with open('./input.txt') as f:
+        lines = f.read().splitlines()
+        Part1(lines)
+        Part2(lines)
